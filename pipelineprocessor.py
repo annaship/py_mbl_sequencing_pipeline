d131ad88aa7da8c84c7fe8e26ad60e9121588e72
annaship <tetya-loshad@yandex.ru>
2012-07-16 16:04:43 -0400

    test

--- pipelineprocessor.py
+++ pipelineprocessor.py
@@ -245,30 +245,30 @@ def env454upload(run, cfg):
 
     my_env454upload = dbUpload(run)
     filenames = my_env454upload.get_fasta_file_names(my_env454upload.fasta_dir)
-#    print "filenames = %s" % filenames
+    print "filenames = %s" % filenames
 
-    for filename in filenames:
-        try:
-            print "FFF: filename = %s" % filename
-            fasta_file_path = my_env454upload.fasta_dir + filename
-            fasta           = u.SequenceSource(fasta_file_path) 
-            filename_base   = filename.split("-")[0]
-            run_info_ill_id = my_env454upload.get_run_info_ill_id(filename_base)
-            gast_dict       = my_env454upload.get_gasta_result(filename)
-
-            while fasta.next():
-                my_env454upload.insert_seq(fasta.seq)
-                my_env454upload.insert_pdr_info(fasta, run_info_ill_id)
-#                my_env454upload.insert_taxonomy(fasta, gast_dict)
-#                my_env454upload.insert_sequence_uniq_info_ill(fasta, gast_dict)
-
-        except Exception, e:          # catch all deriving from Exception (instance e)
-            print "Exception: ", e.__str__()      # address the instance, print e.__str__()
+#    for filename in filenames:
+#        try:
+#            print "FFF: filename = %s" % filename
+#            fasta_file_path = my_env454upload.fasta_dir + filename
+#            fasta           = u.SequenceSource(fasta_file_path) 
+#            filename_base   = filename.split("-")[0]
+#            run_info_ill_id = my_env454upload.get_run_info_ill_id(filename_base)
+#            gast_dict       = my_env454upload.get_gasta_result(filename)
+#
+#            while fasta.next():
+#                my_env454upload.insert_seq(fasta.seq)
+#                my_env454upload.insert_pdr_info(fasta, run_info_ill_id)
+##                my_env454upload.insert_taxonomy(fasta, gast_dict)
+##                my_env454upload.insert_sequence_uniq_info_ill(fasta, gast_dict)
+#
+#        except Exception, e:          # catch all deriving from Exception (instance e)
+#            print "Exception: ", e.__str__()      # address the instance, print e.__str__()
+##            raise                       # re-throw caught exception   
+#        except:                       # catch everything
+#            print "Unexpected:"         # handle unexpected exceptions
+#            print sys.exc_info()[0]     # info about curr exception (type,value,traceback)
 #            raise                       # re-throw caught exception   
-        except:                       # catch everything
-            print "Unexpected:"         # handle unexpected exceptions
-            print sys.exc_info()[0]     # info about curr exception (type,value,traceback)
-            raise                       # re-throw caught exception   
 
     
     # for vamps 'new_lane_keys' will be prefix 
