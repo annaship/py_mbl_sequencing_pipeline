6a9635533bde5f2f34d05ee3ea27311f643cca54
Andy Voorhis - Sogin Lab <avoorhis@bpcweb7.mbl.edu>
2012-07-20 16:30:11 -0400

    added run_tests to start unittesting

---
+++ run_tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+##!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2011, Marine Biological Laboratory
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+import unittest
+
+from test.test_pipeline import TestPipeline
+
+testSuite = TestPipeline.setUpForward()
+
+#text_runner = unittest.TextTestRunner().run(testSuite)
