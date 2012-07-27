e8809e844b5cc44fa70128984dc8fe5ef13b86a9
annaship <tetya-loshad@yandex.ru>
2012-07-27 18:58:30 -0400

    creates correct files and ini

--- pipeline/illumina_files.py
+++ pipeline/illumina_files.py
@@ -32,7 +32,6 @@ class IlluminaFiles:
                 n += 1
                 key_d = dataset + read_n
                 output_file = os.path.join(self.out_file_path, key_d + ".fastq")
-                print "%s: OOO: output_file = %s" % (n, output_file)
                 self.out_files[key_d] = fq.FastQOutput(output_file)
         self.out_files["unknown"] = fq.FastQOutput(os.path.join(self.out_file_path, "unknown" + ".fastq"))
         
@@ -51,7 +50,7 @@ class IlluminaFiles:
         TODO: 1) path should be argument, not hard-coded!
               2) loop through directories, until got files recursively
         """
-        f_in_dir_path  = "/Users/ashipunova/BPC/py_mbl_sequencing_pipeline/test/data/fastq/illumina_files_test/" 
+        f_in_dir_path  = "/Users/ashipunova/BPC/py_mbl_sequencing_pipeline/test/data/fastq/illumina_files_test/illumina_files_test" 
         "TODO: fastq_file_names method to collect all file_names with full path or directories_names"
         (in_files_r1, in_files_r2) = self.get_fastq_file_names(f_in_dir_path)
         self.read1(in_files_r1, compressed)
@@ -71,8 +70,9 @@ input_directory = %s
 output_directory = %s
 
 [files]
-lane_1 = %s
-            """ % (dataset, self.dataset_emails[dataset], f_in_dir_path, f_out_dir_path, dataset+".fastq")
+pair_1 = %s
+pair_2 = %s
+            """ % (dataset, self.dataset_emails[dataset], f_in_dir_path, f_out_dir_path, dataset+"_R1.fastq", dataset+"_R2.fastq")
 #            """ % (dataset, self.dataset_emails[dataset], f_in_dir_path, f_out_dir_path, self.out_files[dataset].file_path)
             ini_file_name = os.path.join(f_out_dir_path,  dataset+".ini")
             self.open_write_close(ini_file_name, text)
@@ -101,11 +101,21 @@ lane_1 = %s
             1) e.pair_no = 1, find run_key -> dataset name
             2) collect the relevant part of id
         """
+        f = 0
         for file_r1 in files_r1:
+            if file_r1 == "/Users/ashipunova/BPC/py_mbl_sequencing_pipeline/test/data/fastq/illumina_files_test/illumina_files_test/Project_Julie_v6_50/Sample_v6_Amplicon_IDX2/v6_Amplicon_IDX2_CGATGT_L004_R1_002.fastq":
+                a = open(file_r1).read()
+                print "III:\n%s" % a
             f_input  = fq.FastQSource(file_r1, compressed)
+            n = 0
+            f += 1
+            print "File: %s" % file_r1
             while f_input.next():
+                n += 1
+
                 e = f_input.entry
                 ini_run_key  = e.index_sequence + "_" + "NNNN" + e.sequence[4:9] + "_" + e.lane_number
+                print "ini_run_key = %s" % ini_run_key
                 if ini_run_key in self.run.samples.keys() and int(e.pair_no) == 1:
                     sample = self.run.samples[ini_run_key] 
                     dataset_file_name = sample.dataset + "_R1"
@@ -116,20 +126,27 @@ lane_1 = %s
                     short_id2 = ":".join(e.header_line.split()[1].split(":")[1:])
                     id2 = short_id1 + " 2:" + short_id2
                     self.id_dataset[id2] = sample.dataset
+                    print "%s-%s: OOO1: e.pair_no = %s, e.header_line = %s,\nid2 = %s\nself.id_dataset[id2] = %s\ne.sequence = %s" % (f, n, e.pair_no, e.header_line, id2, self.id_dataset[id2], e.sequence)
+
                     self.total_seq +=1           
                 else:
                     self.out_files["unknown"].store_entry(e)
                     
     def read2(self, files_r2, compressed):
         "3) e.pair_no = 2, find id from 2), assign dataset_name"
+        f = 0
         for file_r2 in files_r2:
+            print "File: %s" % file_r2
             f_input  = fq.FastQSource(file_r2, compressed)
-
+            n = 0
+            f += 1
             while f_input.next():
+                n += 1
                 e = f_input.entry
                 self.total_seq +=1                
                 
-                if int(e.pair_no) == 2:
+                if (int(e.pair_no) == 2) and (e.header_line in self.id_dataset):
+                    print "%s-%s: OOO2: e.pair_no = %s,\ne.header_line = %s, self.id_dataset[e.header_line] = %s\ne.sequence = %s" % (f, n, e.pair_no, e.header_line, self.id_dataset[e.header_line], e.sequence)
                     file_name = self.id_dataset[e.header_line] + "_R2"
                     self.out_files[file_name].store_entry(e)        
                 else:
