b76702621ca0769547000d31795bbb511f121de2
annaship <tetya-loshad@yandex.ru>
2012-07-31 14:19:30 -0400

    env454 conn

--- pipeline/db_upload.py
+++ pipeline/db_upload.py
@@ -80,8 +80,8 @@ class dbUpload:
 #        self.gast_dir    = os.path.join(run.input_dir, "gast/")
         self.filenames   = []
         "todo: put in args"
-#        self.my_conn     = MyConnection(host = 'newbpcdb2', db="env454")
-        self.my_conn     = MyConnection()    
+        self.my_conn     = MyConnection(host = 'newbpcdb2', db="env454")
+#        self.my_conn     = MyConnection()    
         self.sequence_table_name = "sequence_ill" 
         self.sequence_field_name = "sequence_comp" 
         self.my_csv      = None
