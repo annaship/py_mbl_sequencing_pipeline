94de78835c261c68ed679aa1a77c4dd65a9cbea0
ashipunova <tetya-loshad@yandex.ru>
2012-08-08 20:57:37 -0400

    db upload test; setUpClass

--- test/db_upload_unit_test.py
+++ test/db_upload_unit_test.py
@@ -14,19 +14,20 @@ class DbUloadTestCase(unittest.TestCase):
         cls._connection.execute_no_fetch(msql) 
         msql = "SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';" 
         cls._connection.execute_no_fetch(msql) 
-#        for table_name in ["test.dataset", "test.run_key", "test.run", 
-#                      "test.dna_region", "test.project", "test.dataset", "test.run_info_ill", 
-#                      "test.sequence_ill", "test.sequence_pdr_info_ill", "test.sequence_uniq_info_ill", "test.taxonomy"]:
-#            truncate_test_db_sql = "TRUNCATE %s;" % table_name
-#            cls._connection.execute_no_fetch(truncate_test_db_sql)
-        table_name = "test.dataset"
-#        truncate_test_db_sql = "TRUNCATE test.dataset;"
-        truncate_test_db_sql = "TRUNCATE %s;" % table_name
-        cls._connection.execute_no_fetch(truncate_test_db_sql)
 
+        for table_name in ["test.dataset", "test.run_key", "test.run", 
+                      "test.dna_region", "test.project", "test.dataset", "test.run_info_ill", 
+                      "test.sequence_ill", "test.sequence_pdr_info_ill", "test.sequence_uniq_info_ill", "test.taxonomy"]:
+            truncate_test_db_sql = "TRUNCATE %s;" % table_name
+            cls._connection.execute_no_fetch(truncate_test_db_sql)
+
+        msql = "SET SQL_MODE=@OLD_SQL_MODE"
+        cls._connection.execute_no_fetch(msql) 
+        msql = "SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;"
+        cls._connection.execute_no_fetch(msql) 
+        msql = "SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;"
 #        msql = "SET SQL_MODE=@OLD_SQL_MODE; SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS; SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;"
-#        msql = "SELECT * from dataset"
-#        cls._connection.execute_no_fetch(msql) 
+        cls._connection.execute_no_fetch(msql) 
 #        cls._content = {'182': {'platform': 'Illumina', 'run_key': 'NNNNGCTAC', 'lane': '4', 'run': '20120613', 'IDX': 'GGCTAC', 'dna_region': 'v6', 'vamps_user': 'jreveillaud', 'adaptor': '', 'barcode': '', 'seq_operator': 'JV', 'overlap': 'complete', 'dataset': 'H38', 'project': 'JCR_SPO_Bv6', 'read_length': '101', 'file_prefix': 'H38', 'primer_suite': 'Bacterial v6 Suite', 'tubelabel': 'H38', 'amp_operator': 'JR', 'insert_size': '230'}}
         
     def test_1(self):
