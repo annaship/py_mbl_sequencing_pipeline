bcd26ee0cedc7c4be6034bf3464bcb0f2a1224d2
annaship <tetya-loshad@yandex.ru>
2013-01-04 15:56:13 -0500

    print out for local

--- pipeline/illumina_files.py
+++ pipeline/illumina_files.py
@@ -74,7 +74,7 @@ class IlluminaFiles:
         for idx_key in self.runobj.samples.keys():
             file_name = os.path.join(self.out_file_path, idx_key + ".ini")
             program_name = "analyze-illumina-v6-overlaps"
-            print self.utils.is_local
+            print "self.utils.is_local = %s" % self.utils.is_local
             if self.utils.is_local:
                 program_name = "/Users/ashipunova/bin/illumina-utils/analyze-illumina-v6-overlaps"    
             try:
