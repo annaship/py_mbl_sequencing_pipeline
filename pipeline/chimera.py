b96c4950a1581793d2dc14ed1482c01812ba8bf5
annaship <tetya-loshad@yandex.ru>
2013-06-11 16:08:28 -0400

    get nonchimeras

--- pipeline/chimera.py
+++ pipeline/chimera.py
@@ -226,8 +226,9 @@ class Chimera:
                                         
         uchime_cmd += " --uchimeout "
         uchime_cmd += output_file_name
-        uchime_cmd += " --chimeras "
-        uchime_cmd += (output_file_name + ".chimeras") 
+        uchime_cmd += " --nonchimeras "
+        uchime_cmd += (output_file_name + ".nonchimeras")
+         
         uchime_cmd += cmd_append
 #         print "uchime_cmd FROM create_chimera_cmd = %s" % (uchime_cmd)
         return uchime_cmd
