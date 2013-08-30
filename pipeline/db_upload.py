c22a579d12441e76ac93a85db3036810046a8351
annaship <tetya-loshad@yandex.ru>
2013-08-30 12:31:09 -0400

    os.path.expanduser

--- pipeline/db_upload.py
+++ pipeline/db_upload.py
@@ -46,12 +46,13 @@ class MyConnection:
         self.new_id = None
         self.lastrowid = None
         
-        try:
+        try:           
+            
             print "=" * 40
-            print "host = " + str(host) + ", db = "  + str(db)
+            print "host = " + str(host) + ", db = "  + str(db)            
             print "=" * 40
 
-            self.conn   = MySQLdb.connect(host=host, db=db, read_default_file="~/.my.cnf")
+            self.conn   = MySQLdb.connect(host=host, db=db, read_default_file=os.path.expanduser("~/.my.cnf"))
             self.cursor = self.conn.cursor()
                    
         except MySQLdb.Error, e:
