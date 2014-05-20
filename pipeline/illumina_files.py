9ede173d208b388d27c011f8b8e1fb3cd7ddf5d4
annaship <tetya-loshad@yandex.ru>
2014-05-20 16:54:29 -0400

    remove printout

--- pipeline/illumina_files.py
+++ pipeline/illumina_files.py
@@ -109,7 +109,6 @@ class IlluminaFiles:
             if self.utils.is_local():
                 program_name = C.partial_overlap_cmd_local        
             try:
-                print "PPP1: program_name = %s, --enforce-Q30-check --marker-gene-stringent, ini_file_name = %s" % (program_name, ini_file_name)
                 call([program_name, "--enforce-Q30-check", "--marker-gene-stringent", ini_file_name])           
 #                 call([program_name, ini_file_name])           
 #                 call([program_name, ini_file_name, idx_key])
