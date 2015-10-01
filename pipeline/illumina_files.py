a1344a0f2900c2ea5c5bc3dcbac72da40d498d64
annaship <tetya-loshad@yandex.ru>
2015-10-01 14:52:32 -0400

    rm chmod

--- pipeline/illumina_files.py
+++ pipeline/illumina_files.py
@@ -143,8 +143,8 @@ class IlluminaFiles:
         script_file_name      = self.create_job_array_script(command_line, self.dirs.analysis_dir, file_list)
         script_file_name_full = os.path.join(self.dirs.analysis_dir, script_file_name)
         self.call_sh_script(script_file_name_full, self.dirs.analysis_dir)  
-        self.utils.print_both("self.dirs.chmod_all(%s)" % (self.dirs.analysis_dir))
-        self.dirs.chmod_all(self.dirs.analysis_dir)        
+        # self.utils.print_both("self.dirs.chmod_all(%s)" % (self.dirs.analysis_dir))
+        # self.dirs.chmod_all(self.dirs.analysis_dir)
         return script_file_name              
                           
     def partial_overlap_reads_cluster(self):
@@ -163,8 +163,8 @@ class IlluminaFiles:
         script_file_name      = self.create_job_array_script(command_line, self.dirs.analysis_dir, file_list)
         script_file_name_full = os.path.join(self.dirs.analysis_dir, script_file_name)
         self.call_sh_script(script_file_name_full, self.dirs.analysis_dir)  
-        self.utils.print_both("self.dirs.chmod_all(%s)" % (self.dirs.analysis_dir))
-        self.dirs.chmod_all(self.dirs.analysis_dir)        
+        # self.utils.print_both("self.dirs.chmod_all(%s)" % (self.dirs.analysis_dir))
+        # self.dirs.chmod_all(self.dirs.analysis_dir)
         
         return script_file_name      
                     
@@ -257,8 +257,8 @@ class IlluminaFiles:
         script_file_name      = self.create_job_array_script(command_line, files_dir, file_list)
         script_file_name_full = os.path.join(files_dir, script_file_name)
         self.call_sh_script(script_file_name_full, files_dir)
-        self.utils.print_both("self.dirs.chmod_all(%s)" % (files_dir))
-        self.dirs.chmod_all(files_dir)        
+        # self.utils.print_both("self.dirs.chmod_all(%s)" % (files_dir))
+        # self.dirs.chmod_all(files_dir)
         
         return script_file_name              
 
@@ -296,8 +296,8 @@ class IlluminaFiles:
         script_file_name      = self.create_job_array_script(command_line, files_dir, file_list)
         script_file_name_full = os.path.join(files_dir, script_file_name)
         self.call_sh_script(script_file_name_full, files_dir)  
-        self.utils.print_both("self.dirs.chmod_all(%s)" % (files_dir))
-        self.dirs.chmod_all(files_dir)        
+        # self.utils.print_both("self.dirs.chmod_all(%s)" % (files_dir))
+        # self.dirs.chmod_all(files_dir)
         return script_file_name                           
                                        
     def uniq_fa(self):
