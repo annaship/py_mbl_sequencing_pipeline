ab7a9c6499c43f199050708bb2bb818affcd8e7b
annaship <tetya-loshad@yandex.ru>
2016-01-21 10:01:17 -0500

    not local

--- pipeline/db_upload.py
+++ pipeline/db_upload.py
@@ -152,8 +152,8 @@ class dbUpload:
         database_name = runobj.database_name
         
         self.filenames   = []
-#         self.my_conn     = MyConnection(host = 'newbpcdb2.mbl.edu', db="env454")
-        self.my_conn     = MyConnection()
+        self.my_conn     = MyConnection(host = 'newbpcdb2.mbl.edu', db="env454")
+#         self.my_conn     = MyConnection()
         self.sequence_table_name = "sequence_ill" 
         self.sequence_field_name = "sequence_comp" 
         self.my_csv              = None
