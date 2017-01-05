0cd1be79b4a037e3a2ab955df3fb26c16b1bbbe1
annaship <tetya-loshad@yandex.ru>
2017-01-05 16:43:09 -0500

    Revert "Merge branch 'add_platform'"
    This reverts commit 49ee29aee872617de86b5e2067c2d83d8eda7535, reversing
    changes made to bd54c67f14424b776df712cc1a4c52819fd84441.

--- pipeline/chimera.py
+++ pipeline/chimera.py
@@ -189,7 +189,7 @@ class Chimera:
         regex          = re.compile(r"%s" % tuple_name.find)                                
 #         print "find = %s, replace = %s" % (find, replace)
         if (not tuple_name.cur_file_names) and (tuple_name == from_frequency_to_size):
-            self.utils.print_both('ERROR: Did not find uniqued files ("%s") in %s, please check if the previous step has finished. Exiting.\n' % (C.filtered_suffix + ".unique" , self.indir))
+            self.utils.print_both('ERROR: Did not find uniqued files (".unique") in %s, please check if the previous step has finished. Exiting.\n' % self.indir)
             sys.exit()
         for cur_file_name in tuple_name.cur_file_names:
             file_name = os.path.join(tuple_name.cur_dirname, cur_file_name)           
