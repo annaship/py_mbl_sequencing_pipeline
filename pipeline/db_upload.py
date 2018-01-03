45997464182b29230144d1a7efc038d817279d1c
annaship <tetya-loshad@yandex.ru>
2018-01-03 17:02:26 -0500

    start run_groups

--- pipeline/db_upload.py
+++ pipeline/db_upload.py
@@ -1023,17 +1023,6 @@ class Seq:
         self.utils.print_array_w_title(rows_affected, "rows_affected from insert_sequence_uniq_info = ")
     
     def insert_sequence_uniq_info_ill(self, gast_dict):
-# my_sql = """INSERT IGNORE INTO sequence_uniq_info_ill (%s_id, taxonomy_id, gast_distance, refssu_count, rank_id, refhvr_ids) VALUES
-        """ON DUPLICATE KEY UPDATE
-                       updated = (CASE WHEN taxonomy_id <> %s THEN NOW() ELSE updated END),
-                       taxonomy_id = %s,
-                       gast_distance = '%s',
-                       refssu_count = '%s',
-                       rank_id = %s,
-                       refhvr_ids = '%s';
-                   """  
-#                                       self.table_names["sequence_table_name"], sequence_id, taxonomy_id, distance, refssu_count, rank_id, refhvr_ids.rstrip() 
-
         all_insert_sequence_uniq_info_ill_vals = []
         for fasta_id, gast in gast_dict.items():
             (taxonomy, distance, rank, refssu_count, vote, minrank, taxa_counts, max_pcts, na_pcts, refhvr_ids) = gast
@@ -1062,20 +1051,25 @@ class Seq:
 # self.table_names["sequence_table_name"], sequence_id, taxonomy_id, distance, refssu_count, rank_id, refhvr_ids.rstrip() 
             seq_ins_info = self.my_conn.execute_no_fetch(my_sql)
 
-            
-    
-    def insert_sequence_uniq_info_ill_old(self, gast_dict):
-        all_insert_sequence_uniq_info_ill_sql = []
-        for fasta_id, gast in gast_dict.items():
-            all_insert_sequence_uniq_info_ill_sql.append(self.sequence_uniq_info_ill_query(fasta_id, gast))            
-                     
-        all_insert_sequence_uniq_info_ill_sql_all = " ".join(list(set(all_insert_sequence_uniq_info_ill_sql)))
-        all_insert_sequence_uniq_info_ill_sql_to_run = "BEGIN NOT ATOMIC " + all_insert_sequence_uniq_info_ill_sql_all + "END ; "
+    def run_groups(self, group_vals, query_tmpl):
+        for group in group_vals:
+            val_part = ', '.join([key for key in group if key is not None])
+            my_sql = query_tmpl % (val_part)
+            return self.my_conn.execute_no_fetch(my_sql)
         
-        rows_affected = self.my_conn.cursor.execute(all_insert_sequence_uniq_info_ill_sql_to_run)
-        print "RRR self.cursor._info" 
-        print self.my_conn.cursor._info
     
+#     def insert_sequence_uniq_info_ill_old(self, gast_dict):
+#         all_insert_sequence_uniq_info_ill_sql = []
+#         for fasta_id, gast in gast_dict.items():
+#             all_insert_sequence_uniq_info_ill_sql.append(self.sequence_uniq_info_ill_query(fasta_id, gast))            
+#                      
+#         all_insert_sequence_uniq_info_ill_sql_all = " ".join(list(set(all_insert_sequence_uniq_info_ill_sql)))
+#         all_insert_sequence_uniq_info_ill_sql_to_run = "BEGIN NOT ATOMIC " + all_insert_sequence_uniq_info_ill_sql_all + "END ; "
+#         
+#         rows_affected = self.my_conn.cursor.execute(all_insert_sequence_uniq_info_ill_sql_to_run)
+#         print "RRR self.cursor._info" 
+#         print self.my_conn.cursor._info
+#     
     def sequence_uniq_info_ill_query(self, fasta_id, gast):
         my_sql = ""
         seq  = self.fasta_dict[fasta_id]
