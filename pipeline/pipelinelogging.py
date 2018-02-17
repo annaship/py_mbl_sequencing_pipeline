4c9c0a5eff772d7fbddfba0e62cb9a013b4868ac
annaship <tetya-loshad@yandex.ru>
2018-02-17 15:03:23 -0500

    log file to include run, lane etc.

--- pipeline/pipelinelogging.py
+++ pipeline/pipelinelogging.py
@@ -2,6 +2,7 @@ import logging
 import logging.handlers
 import os
 import sys
+import collections
 
 # create logger with 'spam_application'
 logger = logging.getLogger('')
@@ -18,12 +19,17 @@ FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
 logging.basicConfig(level=logging.DEBUG,format=FORMAT)
 
 running_command_line = " ".join(sys.argv)
+arg_names = sys.argv[1::2]
+arg_vals = sys.argv[2::2]
+args = dict(zip(arg_names, arg_vals))
+
 logger.info("START\n%s", running_command_line)
 # logger.debug("Number of arguments: ", len(sys.argv))
 # logger.info("The arguments are: " , str(sys.argv))
 
+log_file_name = "pipeline_%s_%s_%s_%s.log" % (args['-r'], args['-p'], args['-lane_name'], args['-db_name'])
 # Remove the default FileHandlers if present.
-handler = logging.handlers.RotatingFileHandler(os.path.join(os.getcwd(),"pipeline.log"), maxBytes=10000000, backupCount=10)
+handler = logging.handlers.RotatingFileHandler(os.path.join(os.getcwd(), log_file_name), maxBytes=10000000, backupCount=10)
 logger.addHandler(handler)
 logger.debug("log path: %s" % handler.baseFilename)
 handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
